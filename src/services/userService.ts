30ed13752906c5a2c6c056abb678bf1cfd5e0930
Micael Ferrari <micaelferrari211002@gmail.com>
2025-10-03 00:05:55 -0300

    realizando a melhoria e implementação de validações em funções do arquivo userService

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -5,24 +5,53 @@ import * as usersRepositories from '../repositories/usersRepositories'
 export const getAllUsers = async () =>{
     try{
     const users = await usersRepositories.findAllUsers()
+
+    if(!users || users.length === 0){
+        throw new Error("Nenhum usuário encontrado.")
+    }
     return users;
     }catch(error :any){
-        throw new Error("Erro ao buscar usuário");
+        throw new Error( error.message || "Erro ao buscar usuários.");
     }
 }
 
 export const getUserById = async (id : number)=>{
-    const data = await usersRepositories.findUserById(id);
-    return data;
+    try{
+    const user = await usersRepositories.findUserById(id);
+
+    if(!user){
+        throw new Error("Usuário não encontrado.")
+    }
+
+    return user;
+    } catch (error : any){
+        throw new Error( error.message || "Erro ao buscar usuário.")
+    }
 }
 
 export const createUser = async (id : number, name : string, email : string) =>{
-    //preciso implementar validações
+    try {
+    
+    const userIdExist = await usersRepositories.findUserById(id);
+    //const userEmailExist = await usersRepositories.findUserByEmail(email); // implemetar função para buscar pelo email.
+
+    if(userIdExist){
+        throw new Error("Usuário já existente.")
+    }
+
+    /* if(userEmailExist){
+        throw new Error("Email já cadastrado.")
+    } */
+
     const newUser : usersModel = {id, name, email};
 
     const createUser = await usersRepositories.createUser(newUser);
 
     return createUser;
+        
+    } catch (error : any) {
+         throw new Error( error.message || "Erro ao criar usuário.")
+    }
 }
 
 export const deleteUser = async (id: number) =>{
